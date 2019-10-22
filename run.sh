5a13035da6301662a059862a3c6f16c1cfc1bc39
Kristian Raychev <k.raichev@yahoo.com>
2019-10-22 05:57:19 -0700

    fixes

--- run.sh
+++ run.sh
@@ -2,11 +2,10 @@
 
 echo "Vault password is \"mapr\""
 sleep 5s
-ansible-playbook -vvv -i inventory/hosts --ask-vault-pass ./install-mapr.yml
-ansible -vvv -i inventory/hosts all  -m shell -a " echo stats | nc localhost 5181 | grep Mode"
-ansible-playbook -vvv -i inventory/hosts node1 ./install-license.yml
-ansible-playbook -vvv -i inventory/hosts all ./mount-cluster.yml
+ansible-playbook  -i inventory/hosts --ask-vault-pass ./install-mapr.yml
+ansible  -i inventory/hosts all  -m shell -a " echo stats | nc localhost 5181 | grep Mode"
+ansible-playbook  -i inventory/hosts  ./mount-cluster.yml
 
-ansible -vvv -i inventory/hosts node1 -m shell -a "maprcli node services -cldb restart -nodes node1 node2"
-ansible -vvv -i inventory/hosts node1 -m shell -a "maprcli node services -nfs restart -nodes node1 node2 node3"
+ansible  -i inventory/hosts node1 -m shell -a "maprcli node services -cldb restart -nodes node1 node2"
+ansible  -i inventory/hosts node1 -m shell -a "maprcli node services -nfs restart -nodes node1 node2 node3"
 
